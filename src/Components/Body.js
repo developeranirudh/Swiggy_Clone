f9438748894ff57390098295e0ffa3c06c6768c2
Anirudh gupta <developeranirudhgupta@gmail.com>
2023-06-01 23:06:50 +0530

    Added Live Swiggy's API

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,24 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-import data from "../../ResturantData.json";
 import ResturantCard from "./Resturant.js";
 import { useState } from "react";
-import badge_image from "../../public/images/badge.png"
+import badge_image from "../../public/images/badge.png";
 const Body = () => {
-  const [ResturantList, setResturantList] = useState(data);
+  function filterdata(searchText, resturant) {
+    const searchdata = resturant.filter((resturant) => {
+      if (resturant.data.name.includes(searchText)) {
+        return resturant.data;
+      }
+    });
+    return searchdata;
+  }
 
+  const [ResturantList, setResturantList] = useState([]);
+  const [SearchText, setSearchText] = useState(" ");
+
+  getswiggydata = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0972121&lng=72.9135629&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsondata = await data.json();
+    setResturantList(jsondata?.data?.cards[2]?.data?.data?.cards);
+    console.log(jsondata);
+    return;
+  };
+
+  useEffect(() => {
+    getswiggydata();
+  }, []);
+
+
+  if(ResturantList.length == 0)
+  {
+    return <div> Loading....</div>
+  }
+  else {
   return (
     <>
-    <div className="buttons">
-      <button className="TopRatedResturant"
-        onClick={() => {
-          const filterlist = data.filter((res) => res.data.avgRating >= 4);
-          console.log(parseInt(filterlist[0].data.avgRating))
-          setResturantList(filterlist);
-        }}
-      >
-     <img className="badge_image" src={badge_image} alt="image" />
-      </button>
+      <div className="buttons">
+        <div className="search">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="search-btn"
+            onClick={() => {
+              const searchdata = filterdata(SearchText, ResturantList);
+              setResturantList(searchdata);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="TopRatedResturant"
+          onClick={() => {
+            const filterlist = data.filter((res) => res.data.avgRating >= 4);
+            console.log(parseInt(filterlist[0].data.avgRating));
+            setResturantList(filterlist);
+          }}
+        >
+          <img className="badge_image" src={badge_image} alt="image" />
+        </button>
       </div>
       <div className="res-container">
         {ResturantList.map((resturant) => (
@@ -28,5 +77,5 @@ const Body = () => {
     </>
   );
 };
-
+}
 export default Body;
